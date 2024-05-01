4c6d41f943ea5d34c11c103fc38c9726fd673de2
Abdelbassit-issa <abdelbassit6877@gmail.com>
2024-05-01 11:30:28 +0300

    emergency fix !!

--- wrapperClasses/Main.java
+++ wrapperClasses/Main.java
@@ -3,23 +3,23 @@ import java.io.File;
 
 public class Main {
 
-    // public interface B {
-    //     String run(String r);
+    public interface B {
+        String run(String r);
 
-    // }
+    }
 
     public static void main(String[] args) {
 File file = new File("test.csv");
 
 System.out.println(file.exists());
-        // B b = (s) -> s + "!";
-        // stingFormat("Hello", b);
+        B b = (s) -> s + "!";
+        stingFormat("Hello", b);
         // GuessingGame g = new GuessingGame();
         // g.playGame();
     }
 
-    // public static void stingFormat(String val, B b) {
-    //     String result = b.run(val);
-    //     System.out.println(result);
-    // }
+    public static void stingFormat(String val, B b) {
+        String result = b.run(val);
+        System.out.println(result);
+    }
 }
